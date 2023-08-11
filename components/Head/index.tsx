fbc1557130291f28038d1acf6f17c80f7a62d1ab
Radoslav Stankov <rstankov@gmail.com>
2023-08-11 16:29:46 +0300

    Remove unwanted undefined

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -11,7 +11,7 @@ export default function Head({
   image = '/avatar.jpg',
   description,
 }: IProps) {
-  const url = `https://rstankov.com/${usePathname()}`;
+  const url = `https://rstankov.com/${usePathname() || ''}`;
 
   return (
     <>
